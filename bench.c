aab70739884ce40748a5659a96a2087c730ae452
RunThem <iccy.fun@outlook.com>
2026-01-19 21:44:19 +0800

    test: update benchmark code

--- bench.c
+++ bench.c
@@ -1,12 +1,7 @@
-#define UU_DEBUG
-
 #include "ubench.h"
 #include "uu.h"
 
-#include <assert.h>
-#include <time.h>
-
-const int COUNT = 1000000;
+const int COUNT = 100000;
 
 uu_cmp_fn_def(int, x, y, x - y);
 
@@ -22,25 +17,29 @@ void shuffle(int* array, int len) {
   }
 }
 
+int* make_array(int n) {
+  int* array = (int*)malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    array[i] = i;
+  }
+
+  shuffle(array, n);
+
+  return array;
+}
+
 UBENCH_EX(Dict, at) {
   uu_dict(int, int*) d = uu_dict_init(d, cmp_fn_int);
 
   int i      = 0;
-  int* array = (int*)malloc(COUNT * sizeof(int));
-
-  for (i = 0; i < COUNT; i++) {
-    array[i] = i;
-  }
-
-  shuffle(array, COUNT);
+  int* array = make_array(COUNT);
 
   for (i = 0; i < COUNT; i++) {
     uu_dict_insert(d, array[i], (void*)(intptr_t)array[i]);
   }
 
   UBENCH_DO_BENCHMARK() {
-    shuffle(array, COUNT);
-
     UBENCH_DO_BENCHMARK_BLOCK({
       for (i = 0; i < COUNT; i++) {
         uu_dict_at(d, array[i]);
@@ -59,15 +58,9 @@ UBENCH_EX(Dict, insert) {
   uu_dict(int, int*) d = uu_dict_init(d, cmp_fn_int);
 
   int i      = 0;
-  int* array = (int*)malloc(COUNT * sizeof(int));
-
-  for (i = 0; i < COUNT; i++) {
-    array[i] = i;
-  }
+  int* array = make_array(COUNT);
 
   UBENCH_DO_BENCHMARK() {
-    shuffle(array, COUNT);
-
     UBENCH_DO_BENCHMARK_BLOCK({
       for (i = 0; i < COUNT; i++) {
         uu_dict_insert(d, array[i], (void*)(intptr_t)array[i]);
@@ -90,13 +83,7 @@ UBENCH_EX(Dict, remove) {
   uu_dict(int, int*) d = uu_dict_init(d, cmp_fn_int);
 
   int i      = 0;
-  int* array = (int*)malloc(COUNT * sizeof(int));
-
-  for (i = 0; i < COUNT; i++) {
-    array[i] = i;
-  }
-
-  shuffle(array, COUNT);
+  int* array = make_array(COUNT);
 
   UBENCH_DO_BENCHMARK() {
     for (i = 0; i < COUNT; i++) {
